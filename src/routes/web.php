83d5f3f1e8012ea35c6e3b00ac138ccb94d2a009
abderrazakerrifaouy <abdrzakrifawi@gmail.com>
2026-02-05 23:38:28 +0100

    feat: update login route and add user creation endpoint
    * Rename 'visiteur.login' to 'login' for simplicity
    * Introduce UserController to handle user creation
    * Add 'user.create' endpoint for user registration

--- src/routes/web.php
+++ src/routes/web.php
@@ -3,11 +3,12 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\VisiteurController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\UserController;
 use App\Http\Middleware\IsAdmine;
 
 
 Route::middleware('guest')->group(function () {
-Route::get('/login', [VisiteurController::class, 'login'])->name('visiteur.login');
+Route::get('/login', [VisiteurController::class, 'login'])->name('login');
 Route::post('/login', [VisiteurController::class, 'authenticate'])->name('visiteur.authenticate');
 Route::post('/logout', [VisiteurController::class, 'logout'])->name('visiteur.logout');
 });
@@ -15,4 +16,6 @@ Route::post('/logout', [VisiteurController::class, 'logout'])->name('visiteur.lo
 Route::middleware('auth' , IsAdmine::class)->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'Admine'])->name('dashboard');
     Route::get('/manage_users', [DashboardController::class, 'manageUsers'])->name('manage.users');
+    Route::post('/user/create', [UserController::class, 'create'])->name('user.create');
+
 });
