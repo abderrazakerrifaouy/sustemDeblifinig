3f195a83173106630cc72dc56ae7d11f0e499b97
abderrazakerrifaouy <abdrzakrifawi@gmail.com>
2026-02-06 11:13:44 +0100

    fix: resolve migration conflict with reserved word "class"
    * updated foreign key constraints to use explicit table references
    * renamed "class" table reference to "classes" to avoid conflict

--- src/database/migrations/2026_02_04_194949_formateur_classes.php
+++ src/database/migrations/2026_02_04_194949_formateur_classes.php
@@ -9,11 +9,12 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
+    // fixed errore the class name to avoid conflict with reserved word "class"
     public function up(): void
     {
         Schema::create('formateur_classes', function (Blueprint $table) {
-    $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
-    $table->foreignId('class_id')->constrained('class')->onDelete('cascade');
+    $table->foreignId('user_id')->references('id')->on('users')->onDelete('cascade');
+    $table->foreignId('class_id')->references('id')->on('classes')->onDelete('cascade');
     $table->boolean('is_principal')->default(false); // Pour le Formateur Principal
     $table->primary(['user_id', 'class_id']);
 });
