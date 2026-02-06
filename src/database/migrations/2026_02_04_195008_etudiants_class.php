038537ccb83e34f39b06bff2574a5b45750d3b79
abderrazakerrifaouy <abdrzakrifawi@gmail.com>
2026-02-06 11:13:38 +0100

    fix: resolve migration conflict with reserved word "class"
    * Changed table name from 'class' to 'classes' to avoid conflict
    * Updated foreign key constraints to use correct table name

--- src/database/migrations/2026_02_04_195008_etudiants_class.php
+++ src/database/migrations/2026_02_04_195008_etudiants_class.php
@@ -9,13 +9,14 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
+    // fuxide error the class name to avoid conflict with reserved word "class"
     public function up(): void
     {
         Schema::create('etudiants_class', function (Blueprint $table) {
-    $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
-    $table->foreignId('class_id')->constrained('class')->onDelete('cascade');
+    $table->foreignId('user_id')->references('id')->on('users')->onDelete('cascade');
+    $table->foreignId('class_id')->references('id')->on('classes')->onDelete('cascade');
     $table->primary(['user_id', 'class_id']);
-}); 
+});
     }
 
     /**
