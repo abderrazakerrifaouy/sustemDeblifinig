381494db291c922f038d46b5630c2d053c1439bb
abderrazakerrifaouy <abdrzakrifawi@gmail.com>
2026-02-06 19:34:58 +0100

    feat: update web routes for user and classe management
    * Renamed `UserController@create` to `UserController@store` for consistency
    * Added `ClasseController@store` route for classe creation
    * Added public route for visitor home page

--- src/routes/web.php
+++ src/routes/web.php
@@ -17,8 +17,13 @@ Route::post('/logout', [VisiteurController::class, 'logout'])->name('visiteur.lo
 Route::middleware('auth' , IsAdmine::class)->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'Admine'])->name('dashboard');
     Route::get('/manage_users', [DashboardController::class, 'manageUsers'])->name('manage.users');
-    Route::post('/user/create', [UserController::class, 'create'])->name('user.create');
+    Route::post('/user/create', [UserController::class, 'store'])->name('user.create');
     Route::get('/manage_formations', [DashboardController::class, 'manageFormations'])->name('manage.formations');
     Route::post('/formation/create', [FormationController::class, 'store'])->name('formation.store');
     Route::get('/manage_classes', [DashboardController::class, 'manageClasses'])->name('manage.classes');
+    Route::post('/classe/create', [ClasseController::class, 'store'])->name('classes.store');
 });
+
+
+Route::get('/', [VisiteurController::class, 'home'])->name('user.home');
+
