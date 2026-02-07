99a18c19d88514e248880baa38e330141fca181e
abderrazakerrifaouy <abdrzakrifawi@gmail.com>
2026-02-07 20:56:56 +0100

    feat: add Formation model with relationships

---
+++ src/app/Models/Formation.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class formation extends Model
+{
+    protected $fillable = ['title', 'is_active'];
+
+    public function classes()
+    {
+        return $this->hasMany(ClassModel::class, 'formation_id');
+    }
+    public function commpetonses()
+    {
+        return $this->hasMany(Commpetonse::class, 'formation_id');
+    }
+    public function sprints()
+    {
+        return $this->hasMany(Sprint::class, 'formation_id');
+    }
+}
