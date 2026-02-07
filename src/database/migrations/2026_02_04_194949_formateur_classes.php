48f9371479dd4a20d91a2a6be307524e333ab574
abderrazakerrifaouy <abdrzakrifawi@gmail.com>
2026-02-07 20:57:10 +0100

    refactor: format migration code
    * Removed unnecessary blank lines
    * Reformatted code for better readability
    * Removed trailing whitespace

--- src/database/migrations/2026_02_04_194949_formateur_classes.php
+++ src/database/migrations/2026_02_04_194949_formateur_classes.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,13 +12,12 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('formateur_classes', function (Blueprint $table) {
-    $table->foreignId('user_id')->references('id')->on('users')->onDelete('cascade');
-    $table->foreignId('class_id')->references('id')->on('classes')->onDelete('cascade');
-    $table->boolean('is_principal')->default(false); // Pour le Formateur Principal
-    $table->primary(['user_id', 'class_id']);
-});
+            $table->foreignId('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreignId('class_id')->references('id')->on('classes')->onDelete('cascade');
+            $table->boolean('is_principal')->default(false); // Pour le Formateur Principal
+            $table->primary(['user_id', 'class_id']);
+        });
     }
-
     /**
      * Reverse the migrations.
      */
@@ -28,4 +26,3 @@ return new class extends Migration
         //
     }
 };
-
