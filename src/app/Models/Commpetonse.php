d81c8d4c80670aece1df966b9ae2ee516cabef25
abderrazakerrifaouy <abdrzakrifawi@gmail.com>
2026-02-07 20:57:41 +0100

    refactor: simplify fillable array and fix formation relationship
    * Removed unnecessary array syntax for $fillable
    * Corrected formation relationship to use correct class name and foreign key

--- src/app/Models/Commpetonse.php
+++ src/app/Models/Commpetonse.php
@@ -6,12 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Commpetonse extends Model
 {
-    protected $fillable = [
-        'name',
-        'id_formation',
-    ];
+    protected $fillable = ['name', 'id_formation'];
     public function formation()
     {
-        return $this->belongsTo(formation::class, 'formation_id');
+        return $this->belongsTo(Formation::class, 'formation_id');
     }
 }
